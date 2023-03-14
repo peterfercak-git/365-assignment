2e071f3f997c8227c9bd8fcde46ef798a93952b5
peter.fercak <peter.fercak@visma.com>
2023-03-14 09:36:30 +0100

    destroy build + create ec2 instance

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
     region = "eu-west-2"
 }
 
-resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.0.0/16"
+resource "aws_instance" "app_server" {
+  ami           = "ami-0aaa5410833273cfe"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
